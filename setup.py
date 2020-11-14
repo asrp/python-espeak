5859b35311f3ec26a793dff104914702f704c859
asrp <asrp@email.com>
2020-11-14 11:42:13 -0500

    Update setup package

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-#! /usr/bin/env python
-
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 module = Extension('espeak.core',
 	sources = ['espeak/espeakmodulecore.cpp'],
@@ -10,12 +8,13 @@ setup(
     name = 'python-espeak',
     version = '0.6',
     description = 'Python C extension for the eSpeak speech synthesizer',
-    author = 'Siegfried-A. Gevatter Pujals',
+    author = 'Siegfried-A. Gevatter Pujals, asrp',
     author_email = 'asrp@email.com',
     url = 'https://github.com/asrp/python-espeak',
     license = 'GNU GPL',
     platforms = 'posix',
     ext_modules = [module],
     packages = ['espeak'],
-    long_description = open("README.rst").read(),
+    long_description = open("README.md").read(),
+    long_description_content_type="text/markdown",
     )
